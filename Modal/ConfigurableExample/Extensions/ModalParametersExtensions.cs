1d6f3039bd3d2fd7b0edd2edf7ba28da0fe2fa17
Benoit Hoffman <benoit.hoffman@satellit.be>
2023-11-03 14:35:17 +0100

    Enable different styling possibilities in the ConfigurableExample for Blazored/Modal

--- Modal/ConfigurableExample/Extensions/ModalParametersExtensions.cs
+++ Modal/ConfigurableExample/Extensions/ModalParametersExtensions.cs
@@ -17,4 +17,32 @@ public static class ModalParametersExtensions
             _ => throw new ArgumentOutOfRangeException($"unrecognized value for ContentSize: [{caller.ContentSize}]")
         });
     }
+
+    public static ModalOptions ApplyStyle(this ModalOptions options, ModalSample caller)
+    {
+        options.Class = caller.Style switch
+        {
+            ModalStyle.Default => "",
+            // We keep the default blazored-modal & size-medium for this one because we just build on the existing style for the sake of the example.
+            // Our style in this case only applies to the content, since the body of the modal remains the same
+            ModalStyle.Dark => "blazored-modal size-medium dark-modal", 
+            // That one we fully build from scratch (go check app.css)
+            ModalStyle.Rainbow => "rainbow-modal",
+            _ => throw new ArgumentOutOfRangeException($"unrecognized value for ModalOptions: [{caller.Style}]")
+        };
+        return options;
+    }
+
+    public static ModalOptions ApplyAnimation(this ModalOptions options, ModalSample caller)
+    {
+        options.AnimationType = caller.ShouldFadeIn ? ModalAnimationType.FadeInOut : ModalAnimationType.None;
+        return options;
+    }
+
+
+    public static ModalOptions ApplyOverlay(this ModalOptions options, ModalSample caller)
+    {
+        options.OverlayCustomClass = caller.ShouldUseDefaultOverlay ? "" : "custom-modal-overlay";
+        return options;
+    }
 }
