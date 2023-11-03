488c289555adf0da5690c51ed9f55a40ceb70cd4
Benoit Hoffman <benoit.hoffman@satellit.be>
2023-11-03 09:28:13 +0100

    Setup properly the page to base cofiguration of a modal on

---
+++ Modal/ConfigurableExample/Extensions/ModalParametersExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Blazored.Modal;
+using ConfigurableExample.Components.Modals;
+using ConfigurableExample.Components.Pages;
+using static ConfigurableExample.Components.Pages.ModalSample;
+
+namespace ConfigurableExample.Extensions;
+
+public static class ModalParametersExtensions
+{
+    public static ModalParameters AddContentSize(this ModalParameters param, ModalSample caller)
+    {
+        return param.Add(nameof(SimpleModal.Message), caller._contentSize switch
+        {
+            ModalContentSize.Small => caller._smallContent,
+            ModalContentSize.Normal => caller._normalContent,
+            ModalContentSize.Huge => caller._hugeContent,
+            _ => throw new ArgumentOutOfRangeException($"unrecognized value for ContentSize: [{caller._contentSize}]")
+        });
+    }
+}
--- Modal/ConfigurableExample/Program.cs
+++ Modal/ConfigurableExample/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using ConfigurableExample.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazoredModal();
 
 await builder.Build().RunAsync();
