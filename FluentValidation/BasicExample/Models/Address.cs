c73c1b7c444aba0dbe7f53142eeff760619c0015
Benoit Hoffman <benoit.hoffman@satellit.be>
2023-12-01 10:59:44 +0100

    Created empty project to prepare for fluent validation. Has base models, form and validator already

---
+++ FluentValidation/BasicExample/Models/Address.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace BasicExample.Models
+{
+    public class Address
+    {
+        public string StreetName { get; set; }
+        public int StreetNumber { get; set; }
+    }
+
+    public class AddressValidator : AbstractValidator<Address>
+    {
+        public AddressValidator()
+        {
+            RuleFor(x => x.StreetName)
+                .NotEmpty();
+            RuleFor(x => x.StreetNumber)
+                .NotEmpty()
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ FluentValidation/BasicExample/Models/Person.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace BasicExample.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Address Address { get; set; } = new Address();
+    }
+
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleSet("Names", () =>
+            {
+                RuleFor(x => x.FirstName)
+                    .NotEmpty();
+                RuleFor(x => x.LastName)
+                    .NotEmpty();
+            });
+
+            RuleFor(x => x.Age)
+                .NotEmpty()
+                .ExclusiveBetween(0, 150);
+
+            RuleFor(x => x.Address)
+                .SetValidator(new AddressValidator());
+        }
+    }
+}
